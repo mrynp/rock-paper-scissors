b7d041968d26b06158c4b92a74f17e5970030228
mrynp <110476086+mrynp@users.noreply.github.com>
2024-08-20 16:13:50 +0800

    Add play again button at the end of match

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ const game = () => {
   // Play match
 
   const playMatch = () => {
-    const choices = document.querySelectorAll(".choices button");
     const playerHand = document.querySelector(".player-hand");
     const computerHand = document.querySelector(".computer-hand");
+    const choices = document.querySelectorAll(".choices button");
     const hands = document.querySelectorAll(".hands img");
 
     hands.forEach((hand) => {
@@ -46,27 +46,62 @@ const game = () => {
           // Update images
           playerHand.src = `./assets/${this.textContent}.png`;
           computerHand.src = `./assets/${computerChoice}.png`;
-        }, 2000);
+        }, 1500);
 
         // Animation
-        playerHand.style.animation = "shakePlayer 2s ease";
-        computerHand.style.animation = "shakeComputer 2s ease";
+        playerHand.style.animation = "shakePlayer 1.5s ease";
+        computerHand.style.animation = "shakeComputer 1.5s ease";
       })
     );
   };
 
+  const result = document.querySelector(".result");
+  const choices = document.querySelector(".choices");
+  const playerScore = document.querySelector(".player-score");
+  const computerScore = document.querySelector(".computer-score");
+  const playAgain = document.querySelector(".outro button");
+
+  let winningScore = 5;
+  let isGameOver = false;
+
   const updateScore = () => {
-    const playerScore = document.querySelector(".player-score");
-    const computerScore = document.querySelector(".computer-score");
+    if (!isGameOver) {
+      if (pScore === winningScore) {
+        isGameOver = true;
+        result.textContent = "YOU WIN!!!";
+        playAgain.style.display = "block";
+        choices.style.display = "none";
+
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+      } else if (cScore === winningScore) {
+        isGameOver = true;
+        result.textContent = "YOU LOST!!!";
+        playAgain.style.display = "block";
+        choices.style.display = "none";
+
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+      } else {
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+      }
+    }
+  };
+
+  playAgain.addEventListener("click", () => {
+    isGameOver = false;
+    choices.style.display = "flex";
+    playAgain.style.display = "none";
+    result.textContent = "Choose an option";
+    pScore = 0;
+    cScore = 0;
     playerScore.textContent = pScore;
     computerScore.textContent = cScore;
-    console.log(`player: ${pScore}`);
-    console.log(`player: ${cScore}`);
-  };
+  });
 
   const getResult = (playerChoice, computerChoice) => {
     // Update text
-    const result = document.querySelector(".result");
 
     if (playerChoice === computerChoice) {
       result.textContent = "It is a tie";
